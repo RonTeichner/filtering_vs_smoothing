7748aeebaf3dc3087fb042b79ed23db5edf05a05
Ron Teichner <ron.teichner@campus.technion.ac.il>
2020-11-19 14:20:55 +0200

    Pytorch filtering and smoothing identical to Anderson

--- Anderson_vs_pytorch.py
+++ Anderson_vs_pytorch.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.cm as cm
 from pytorchKalman_func import *
+from analyticResults_func import watt2db
 
 np.random.seed(11)
 dim_x, dim_z = 5, 3
 N = 1000  # time steps
-batchSize = 16
+batchSize = 1
 
 # estimator init values:
 filter_P_init = np.repeat(np.eye(dim_x)[None, None, :, :], batchSize, axis=1)  # filter @ time-series but all filters have the same init
@@ -22,8 +23,26 @@ sysModel = GenSysModel(dim_x, dim_z)
 # create time-series measurements (#time-series == batchSize):
 z, _ = GenMeasurements(N, batchSize, sysModel) # z: [N, batchSize, dim_z]
 
-x_est_f, x_est_s = Anderson_filter_smoother(z, sysModel, filter_P_init, filterStateInit)
+# run Anderson's filter & smoother:
+x_est_f_A, x_est_s_A = Anderson_filter_smoother(z, sysModel, filter_P_init, filterStateInit)
 # x_est_f[k] has the estimation of x[k] given z[0:k-1]
 # x_est_s[k] has the estimation of x[k] given z[0:N-1]
 
-x=3
+# run pytorch filter & smoother:
+# (filter_P_init is not in use because this filter works from the start on the steady-state-gain)
+x_est_f_P, x_est_s_P = Pytorch_filter_smoother(z, sysModel, filterStateInit)
+
+x_est_f_P = x_est_f_P.cpu().numpy()
+x_est_s_P = x_est_s_P.cpu().numpy()
+
+filtering_recursiveAnderson_recursivePytorch_diff_energy = watt2db(np.power(np.linalg.norm(x_est_f_A[:, 0] - x_est_f_P[:, 0], axis=1), 2))
+smoothing_recursiveAnderson_recursivePytorch_diff_energy = watt2db(np.power(np.linalg.norm(x_est_s_A[:, 0] - x_est_s_P[:, 0], axis=1), 2))
+
+plt.figure(figsize=(16, 8))
+plt.plot(filtering_recursiveAnderson_recursivePytorch_diff_energy, label='Filtering')
+plt.plot(smoothing_recursiveAnderson_recursivePytorch_diff_energy, label='Smoothing')
+plt.title(r'Anderson vs Pytorch')
+plt.ylabel('db')
+plt.legend()
+plt.grid()
+plt.show()
--- pytorchKalman_func.py
+++ pytorchKalman_func.py
@@ -4,6 +4,7 @@ from filterpy.kalman import KalmanFilter, update, predict, batch_filter
 from filterpy.common import Q_discrete_white_noise, kinematic_kf, Saver
 from scipy.linalg import solve_discrete_are, solve_discrete_lyapunov
 from scipy.spatial.distance import mahalanobis as scipy_mahalanobis
+import torch
 
 def GenSysModel(dim_x, dim_z):
     if dim_x == 1:
@@ -43,4 +44,74 @@ def Anderson_filter_smoother(z, sysModel, filter_P_init, filterStateInit):
     # filter_P_init: [1, batchSize, dim_x, dim_x]
     # filterStateInit: [1, batchSize, dim_x, 1]
     # z: [N, batchSize, dim_z, 1]
-    
+    F, H, Q, R = sysModel["F"], sysModel["H"], sysModel["Q"], sysModel["R"]
+    dim_x, dim_z = F.shape[0], H.shape[1]
+    N, batchSize = z.shape[0], z.shape[1]
+
+    # define estimator
+    k_filter = KalmanFilter(dim_x=dim_x, dim_z=dim_z)
+    k_filter.Q, k_filter.R, k_filter.H, k_filter.F = Q, R, H.transpose(), F
+
+    # run estimator on measurements:
+    x_est_f, x_est_s = np.zeros((N, batchSize, dim_x, 1)), np.zeros((N, batchSize, dim_x, 1))
+    for b in range(batchSize):
+        k_filter.x, k_filter.P = filterStateInit[0, b].copy(), filter_P_init[0, b].copy()
+        x_est, cov, x_est_f_singleTimeSeries, _ = k_filter.batch_filter(zs=z[:, b], update_first=False)
+        x_est_s_singleTimeSeries, _, _, _ = k_filter.rts_smoother(x_est, cov)
+        x_est_f[:, b], x_est_s[:, b] = x_est_f_singleTimeSeries, x_est_s_singleTimeSeries
+
+    return x_est_f, x_est_s
+
+def Pytorch_filter_smoother(z, sysModel, filterStateInit):
+    # filter_P_init: [1, batchSize, dim_x, dim_x] is not in use because this filter works from the start on the steady-state-gain
+    # filterStateInit: [1, batchSize, dim_x, 1]
+    # z: [N, batchSize, dim_z, 1]
+    F, H, Q, R = sysModel["F"], sysModel["H"], sysModel["Q"], sysModel["R"]
+    dim_x, dim_z = F.shape[0], H.shape[1]
+    N, batchSize = z.shape[0], z.shape[1]
+
+    theoreticalBarSigma = solve_discrete_are(a=np.transpose(F), b=H, q=Q, r=R)
+    Ka_0 = np.dot(theoreticalBarSigma, np.dot(H, np.linalg.inv(np.dot(np.transpose(H), np.dot(theoreticalBarSigma, H)) + R)))  # first smoothing gain
+    K = np.dot(F, Ka_0) # steadyKalmanGain
+    tildeF = F - np.dot(K, np.transpose(H))
+    Sint = np.matmul(np.linalg.inv(np.matmul(F, theoreticalBarSigma)), K)
+    thr = 1e-20 * np.abs(tildeF).max()
+
+    # stuff to cuda:
+    tildeF = torch.tensor(tildeF, dtype=torch.float).cuda()
+    tildeF_transpose = torch.transpose(tildeF, 1, 0)
+    K = torch.tensor(K, dtype=torch.float).cuda()
+    filterStateInit = torch.tensor(filterStateInit, dtype=torch.float).cuda()
+    z = torch.tensor(z, dtype=torch.float).cuda()
+    H = torch.tensor(H, dtype=torch.float).cuda()
+    Sint = torch.tensor(Sint, dtype=torch.float).cuda()
+    H_transpose = torch.transpose(H, 1, 0)
+    thr = torch.tensor(thr, dtype=torch.float).cuda()
+    theoreticalBarSigma = torch.tensor(theoreticalBarSigma, dtype=torch.float).cuda()
+
+    # filtering, inovations:
+    hat_x_k_plus_1_given_k = torch.zeros(N, batchSize, dim_x, 1, dtype=torch.float).cuda()  # hat_x_k_plus_1_given_k is in index [k+1]
+    bar_z_k = torch.zeros(N, batchSize, dim_z, 1, dtype=torch.float).cuda()
+    hat_x_k_plus_1_given_k[0] = torch.matmul(tildeF, filterStateInit[0]) + torch.matmul(K, z[0])
+    bar_z_k[0] = z[0]
+    for k in range(N-1):
+        hat_x_k_plus_1_given_k[k+1] = torch.matmul(tildeF, hat_x_k_plus_1_given_k[k]) + torch.matmul(K, z[k])
+    for k in range(N):
+        bar_z_k[k] = z[k] - torch.matmul(H_transpose, hat_x_k_plus_1_given_k[k])
+
+    # smoothing:
+    hat_x_k_given_N = torch.zeros(N, batchSize, dim_x, 1, dtype=torch.float).cuda()
+    for k in range(N):
+        # for i==k:
+        Ka_i_minus_k = torch.matmul(theoreticalBarSigma, Sint)
+        hat_x_k_given_i = hat_x_k_plus_1_given_k[k] + torch.matmul(Ka_i_minus_k, bar_z_k[k])
+        for i in range(k+1, N):
+            Ka_i_minus_k = torch.matmul(theoreticalBarSigma, torch.matmul(torch.matrix_power(tildeF_transpose, i-k), Sint))
+            hat_x_k_given_i = hat_x_k_given_i + torch.matmul(Ka_i_minus_k, bar_z_k[i])
+
+            if torch.max(torch.abs(Ka_i_minus_k)) < thr:
+                break
+        hat_x_k_given_N[k] = hat_x_k_given_i
+
+    #  x_est_f, x_est_s =  hat_x_k_plus_1_given_k, hat_x_k_given_N - these are identical values
+    return hat_x_k_plus_1_given_k, hat_x_k_given_N
