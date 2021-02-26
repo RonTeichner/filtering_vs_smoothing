382e574fdb03b76e744f2236fa918239b3d7da72
Ron Teichner <ron.teichner@campus.technion.ac.il>
2021-02-27 00:14:30 +0200

    dictionary stores eigenvalues

--- pytorchKalman_func.py
+++ pytorchKalman_func.py
@@ -460,6 +460,7 @@ class playersToolbox:
         self.compute_bar_Xi_N_bar_Xi_N_transpose_previous = 0
         self.compute_J_j_N_previous_j, self.compute_J_j_N_previous_N = 0, 0
         self.compute_J_j_N_eig_previous_j, self.compute_J_j_N_eig_previous_N = 0, 0
+        self.compute_J_j_N_eig_dict = dict()
         self.compute_tildeJ_j_N_previous_j, self.compute_tildeJ_j_N_previous_N = 0, 0
         self.compute_tildeJ_N0_j_N_previous_N_0, self.compute_tildeJ_N0_j_N_previous_j, self.compute_tildeJ_N0_j_N_previous_N = 0, 0, 0
         self.compute_L_j_N_previous_j, self.compute_L_j_N_previous_N = 0, 0
@@ -928,9 +929,13 @@ class playersToolbox:
         return (J_j_N, J_j_N_param_j, J_j_N_param_N), b, alpha
 
     def compute_J_j_N_eigenvalues(self, j, N):
-        if not (j == self.compute_J_j_N_eig_previous_j and N == self.compute_J_j_N_eig_previous_N):
-            self.compute_J_j_N_eig_previous_j, self.compute_J_j_N_eig_previous_N = j, N
-            self.J_j_N_eig = torch.symeig(self.compute_J_j_N(j, N), eigenvectors=True)
+        #if not (j == self.compute_J_j_N_eig_previous_j and N == self.compute_J_j_N_eig_previous_N):
+        #    self.compute_J_j_N_eig_previous_j, self.compute_J_j_N_eig_previous_N = j, N
+        #    self.J_j_N_eig = torch.symeig(self.compute_J_j_N(j, N), eigenvectors=True)
+        key = 'j == ' + str(j) + '; N == ' + str(N)
+        if key not in self.compute_J_j_N_eig_dict:
+            self.compute_J_j_N_eig_dict[key] =  torch.symeig(self.compute_J_j_N(j, N), eigenvectors=True)
+        self.J_j_N_eig = self.compute_J_j_N_eig_dict[key]
         return self.J_j_N_eig
 
 
@@ -1302,7 +1307,7 @@ def runBoundSimulation(sysModel, useCuda, enableSmartPlayers, N, mistakeBound, d
         caligraphE_F_minus_1_b, caligraphE_F_minus_1_mean_batch, \
         caligraphE_F_0_b, caligraphE_F_1_b, caligraphE_F_2_b, caligraphE_F_3_b, \
         caligraphE_F_0_mean_batch, caligraphE_F_1_mean_batch, caligraphE_F_2_mean_batch, caligraphE_F_3_mean_batch, bounds_batch\
-            = runBoundSimBatch(N, minBatchSize, sysModel, useCuda, pytorchEstimator, adversarialPlayersToolbox, dim_x, enableSmartPlayers, enableCausalPlayer)
+            = runBoundSimBatch(batchIter == 0, N, minBatchSize, sysModel, useCuda, pytorchEstimator, adversarialPlayersToolbox, dim_x, enableSmartPlayers, enableCausalPlayer)
 
         if batchIter == 0:
             caligraphE_F_minus_1_mean, caligraphE_F_0_mean, caligraphE_F_1_mean, caligraphE_F_2_mean, caligraphE_F_3_mean, bounds = caligraphE_F_minus_1_mean_batch, caligraphE_F_0_mean_batch, caligraphE_F_1_mean_batch, caligraphE_F_2_mean_batch, caligraphE_F_3_mean_batch, bounds_batch
@@ -1330,7 +1335,7 @@ def runBoundSimulation(sysModel, useCuda, enableSmartPlayers, N, mistakeBound, d
 
     return bounds, fileName
 
-def runBoundSimBatch(N, batchSize, sysModel, useCuda, pytorchEstimator, adversarialPlayersToolbox, dim_x, enableSmartPlayers, enableCausalPlayer):
+def runBoundSimBatch(dp, N, batchSize, sysModel, useCuda, pytorchEstimator, adversarialPlayersToolbox, dim_x, enableSmartPlayers, enableCausalPlayer):
     # create time-series measurements (#time-series == batchSize):
     tilde_z, tilde_x, processNoises, measurementNoises = GenMeasurements(N, batchSize, sysModel)  # z: [N, batchSize, dim_z]
     tilde_z, tilde_x, processNoises, measurementNoises = torch.tensor(tilde_z, dtype=torch.float), torch.tensor(tilde_x, dtype=torch.float), torch.tensor(processNoises, dtype=torch.float), torch.tensor(measurementNoises, dtype=torch.float)
@@ -1340,7 +1345,7 @@ def runBoundSimBatch(N, batchSize, sysModel, useCuda, pytorchEstimator, adversar
     # estimator init values:
     filter_P_init = pytorchEstimator.theoreticalBarSigma.cpu().numpy()  # filter @ time-series but all filters have the same init
     filterStateInit = np.matmul(np.linalg.cholesky(filter_P_init), np.random.randn(batchSize, dim_x, 1))
-    print(f'filter init mean error energy w.r.t trace(bar(sigma)): {watt2dbm(np.mean(np.power(np.linalg.norm(filterStateInit, axis=1), 2), axis=0)) - watt2dbm(np.trace(filter_P_init))} db')
+    if dp: print(f'filter init mean error energy w.r.t trace(bar(sigma)): {watt2dbm(np.mean(np.power(np.linalg.norm(filterStateInit, axis=1), 2), axis=0)) - watt2dbm(np.trace(filter_P_init))} db')
     filterStateInit = torch.tensor(filterStateInit, dtype=torch.float, requires_grad=False).contiguous()
     # filterStateInit = tilde_x[0]
 
@@ -1359,7 +1364,7 @@ def runBoundSimBatch(N, batchSize, sysModel, useCuda, pytorchEstimator, adversar
     tilde_e_k_given_k_minus_1 = tilde_x - tilde_x_est_f  # k is the index so that at tilde_e_k_given_k_minus_1[0] we have tilde_e_0_given_minus_1
     tilde_e_k_given_N_minus_1 = tilde_x - tilde_x_est_s
 
-    print(f'mean energy of tilde_x: ', {watt2dbm(calcTimeSeriesMeanEnergy(tilde_x).mean())}, ' [dbm]')
+    if dp: print(f'mean energy of tilde_x: ', {watt2dbm(calcTimeSeriesMeanEnergy(tilde_x).mean())}, ' [dbm]')
 
     # the next code checks the error expression used by the causal player
     # tilde_e_k_given_k_minus_1_directCalc = adversarialPlayersToolbox.test_tilde_e_expression(tilde_x[0:1], filterStateInit, processNoises, measurementNoises, tilde_e_k_given_k_minus_1)
@@ -1370,7 +1375,7 @@ def runBoundSimBatch(N, batchSize, sysModel, useCuda, pytorchEstimator, adversar
         u_0 = u_0.cuda()
     u_0 = noKnowledgePlayer(u_0)
 
-    print(f'mean energy of u_0: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_0).mean())}, ' [dbm]')
+    if dp: print(f'mean energy of u_0: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_0).mean())}, ' [dbm]')
 
     # Kalman filters:
     z_0 = tilde_z + torch.matmul(H_transpose, u_0)
@@ -1392,23 +1397,20 @@ def runBoundSimBatch(N, batchSize, sysModel, useCuda, pytorchEstimator, adversar
         if enableTestEnergyFactor:
             u_3_doubleEnergy = torch.zeros(N, batchSize, dim_x, 1, dtype=torch.float)
             u_3_doubleEnergy = geniePlayer(adversarialPlayersToolbox, u_3_doubleEnergy, tilde_e_k_given_k_minus_1, 2)
-            print(f'mean energy of u_3: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_3).mean())}, ' [dbm]')
-            print(f'mean energy of u_3_doubleEnergy: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_3_doubleEnergy).mean())},
-                  ' [dbm]')
+            if dp: print(f'mean energy of u_3: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_3).mean())}, ' [dbm]')
+            if dp: print(f'mean energy of u_3_doubleEnergy: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_3_doubleEnergy).mean())}, ' [dbm]')
             plt.figure()
             batchIdx = 0
-            plt.plot(volt2dbm(np.linalg.norm(u_3[:, batchIdx:batchIdx + 1].cpu().numpy(), axis=2))[:, 0, 0] - volt2dbm(
-                np.linalg.norm(u_3_doubleEnergy[:, batchIdx:batchIdx + 1].cpu().numpy(), axis=2))[:, 0, 0],
-                     label=r'$\frac{||u_N(N)^{(3)}||_2}{||u_N(2N)^{(3)}||_2}$')
+            plt.plot(volt2dbm(np.linalg.norm(u_3[:, batchIdx:batchIdx + 1].cpu().numpy(), axis=2))[:, 0, 0] - volt2dbm(np.linalg.norm(u_3_doubleEnergy[:, batchIdx:batchIdx + 1].cpu().numpy(), axis=2))[:, 0, 0], label=r'$\frac{||u_N(N)^{(3)}||_2}{||u_N(2N)^{(3)}||_2}$')
             plt.ylabel('db')
             plt.xlabel('k')
             plt.grid()
             plt.legend()
             # plt.show()
 
-        print(f'mean energy of u_1: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_1).mean())}, ' [dbm]')
-        print(f'mean energy of u_2: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_1).mean())}, ' [dbm]')
-        print(f'mean energy of u_3: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_3).mean())}, ' [dbm]')
+        if dp: print(f'mean energy of u_1: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_1).mean())}, ' [dbm]')
+        if dp: print(f'mean energy of u_2: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_1).mean())}, ' [dbm]')
+        if dp: print(f'mean energy of u_3: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_3).mean())}, ' [dbm]')
 
         z_1 = tilde_z + torch.matmul(H_transpose, u_1)
         x_1_est_f, x_1_est_s = pytorchEstimator(z_1, filterStateInit)
@@ -1423,11 +1425,11 @@ def runBoundSimBatch(N, batchSize, sysModel, useCuda, pytorchEstimator, adversar
         noPlayerBound, noKnowledgePlayerBound, noAccessPlayerBound, causlaPlayerBound, geniePlayerBound = computeBounds(tilde_x, tilde_x_est_f, x_0_est_f, x_1_est_f, x_2_est_f, x_3_est_f)
         bounds = (noPlayerBound, noKnowledgePlayerBound, noAccessPlayerBound, causlaPlayerBound, geniePlayerBound)
 
-        print(f'mean energy of tilde_x: ', {watt2dbm(calcTimeSeriesMeanEnergy(tilde_x).mean())}, ' [dbm]')
-        print(f'mean energy of u_0: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_0).mean())}, ' [dbm]')
-        print(f'mean energy of u_1: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_1).mean())}, ' [dbm]')
-        print(f'mean energy of u_2: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_1).mean())}, ' [dbm]')
-        print(f'mean energy of u_3: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_3).mean())}, ' [dbm]')
+        if dp: print(f'mean energy of tilde_x: ', {watt2dbm(calcTimeSeriesMeanEnergy(tilde_x).mean())}, ' [dbm]')
+        if dp: print(f'mean energy of u_0: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_0).mean())}, ' [dbm]')
+        if dp: print(f'mean energy of u_1: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_1).mean())}, ' [dbm]')
+        if dp: print(f'mean energy of u_2: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_1).mean())}, ' [dbm]')
+        if dp: print(f'mean energy of u_3: ', {watt2dbm(calcTimeSeriesMeanEnergy(u_3).mean())}, ' [dbm]')
 
         N, batchSize, dim_x = tilde_x.shape[0], tilde_x.shape[1], tilde_x.shape[2]
 
